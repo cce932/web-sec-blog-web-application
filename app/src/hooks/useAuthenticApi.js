71cd50c788071e78e091f2abd0205c655d3a47ca
靖宜 陳 <b10709032@gapps.ntust.edu.tw>
2022-04-19 22:36:30 +0800

    fe feat: build useAuthenticApi

---
+++ app/src/hooks/useAuthenticApi.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import { useProfileContext } from './useProfile';
+
+const useAuthenticApi = () => {
+  const { profile } = useProfileContext();
+
+  const instance = axios.create({
+    baseURL: process.env.REACT_APP_API,
+    timeout: 1000,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Request-Headers': 'Authorization',
+    },
+  });
+
+  instance.defaults.headers.common['Authorization'] = profile.id; // or directly define in headers when creating
+
+  return instance;
+};
+export default useAuthenticApi;
