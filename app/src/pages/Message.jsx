ffd37df124d993a25ebbf66323d82c55cb872a67
靖宜 陳 <b10709032@gapps.ntust.edu.tw>
2022-04-20 20:57:27 +0800

    fe feat: redirect to login when not logged in

--- app/src/pages/Message.jsx
+++ app/src/pages/Message.jsx
@@ -1,32 +1,42 @@
 import React, { useState } from 'react';
-import { Box, Button } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import { useNavigate, useParams, useLocation } from 'react-router-dom';
 import MessageComponent from '../components/Message';
 import { useEffect } from 'react';
 import useAuthenticApi from '../hooks/useAuthenticApi';
+import { useProfileContext } from '../hooks/useProfile';
 
 const Message = () => {
+  const { profile } = useProfileContext();
   const { id } = useParams();
   const authApi = useAuthenticApi();
   const [message, setMessage] = useState({});
   const navigate = useNavigate();
   const location = useLocation();
 
-  console.log(location.pathname);
   useEffect(() => {
-    authApi
-      .get(`/message/get.php?id=${id}`)
-      .then(({ data }) => {
-        setMessage(data);
-      })
-      .catch((error) => {
-        console.log('error', error);
-        if (location.pathname.includes('/message/')) navigate('/');
-      });
+    // check if profile exists
+    if (profile?.id) {
+      authApi
+        .get(`/message/get.php?id=${id}`)
+        .then(({ data }) => {
+          setMessage(data);
+        })
+        .catch((error) => {
+          console.log('error', error);
+          if (location.pathname.includes('/message/')) navigate('/');
+        });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  if (!Object.keys(message).length) return <Box sx={{ p: 4 }}>Loading...</Box>;
+  if (!Object.keys(message).length)
+    return (
+      <Typography variant="h5" color="primary" sx={{ textAlign: 'center' }}>
+        Loading...
+      </Typography>
+    );
 
   return (
     <Box sx={{ p: 4 }}>
