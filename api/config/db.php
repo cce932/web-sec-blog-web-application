a572d35e0e8f332342466369ed23f88c180349ab
cce932 <cchingyiii@gmail.com>
2022-06-11 05:00:19 +0000

    cicd

--- api/config/db.php
+++ api/config/db.php
@@ -1,7 +1,11 @@
 <?php
+// define('DB_HOST', getenv('DB_HOST'));
+// define('DB_USER', getenv('DB_USER'));
+// define('DB_PASS', getenv('DB_PASS'));
+// define('DB_NAME', getenv('DB_NAME'));
 define('DB_HOST', 'db');
-define('DB_USER', 'user');
-define('DB_PASS', 'PASSWORD_test');
+define('DB_USER', 'denaroot');
+define('DB_PASS', 'denaroot');
 define('DB_NAME', 'myDb');
  
 // connect in each file, not here
--- api/config/header.php
+++ api/config/header.php
@@ -1,5 +1,5 @@
 <?php
-header('Access-Control-Allow-Origin: http://localhost:3000');
+header('Access-Control-Allow-Origin: http://mid.dena.software');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS, DELETE, PUT');
 header('Access-Control-Allow-Headers: Origin, Method, Content-type, Authorization');
 header('Access-Control-Expose-Headers: Origin, Method, Content-type');
